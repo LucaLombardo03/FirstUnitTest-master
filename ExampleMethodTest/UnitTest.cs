c46ce6933ee3a71fd3b7a96bb49f69c94f9a3963
luca lombardo <lucalombardo03@gmail.com>
2019-11-01 12:24:27 +0100

    Add project files.

---
+++ ExampleMethodTest/UnitTest.cs
@@ -0,0 +1,39 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TextExample_1;
+
+namespace ExampleMethodTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestCalcolo1()
+        {
+            {
+                bool resp = Method.Calcolo1();
+                Assert.AreEqual(true, resp);
+            }
+        }
+        public void TestCalcolo2()
+        {
+            {
+                bool resp = Method.Calcolo2();
+                Assert.AreEqual(false, resp);
+            }
+        }
+        public void TestCalcolo3()
+        {
+            {
+                bool resp = Method.Calcolo3();
+                Assert.AreEqual(true, resp);
+            }
+        }
+        public void TestCalcolo4()
+        {
+            {
+                bool resp = Method.Calcolo4();
+                Assert.AreEqual(false, resp);
+            }
+        }
+    }
+}
---
+++ TextExample_1/Method.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace TextExample_1
+{
+    public class Method
+    {
+        public static bool Calcolo1()
+        {
+            double a = 1.0 / 10.0;
+            double s = a + a + a + a + a + a + a + a + a + a;
+            if (s == 1)
+                return true;
+            else
+                return false;
+        }
+        public static bool Calcolo2()
+        {
+            double a = 1.0 / 10.0;
+            double s = a + a + a + a + a + a + a + a + a + a;
+            if (s == 1)
+                return false;
+            else
+                return true;
+        }
+        public static bool Calcolo3()
+        {
+            double a = 1.0 / 6.0;
+            double prod = a * 6.0;
+            double s = a + a + a + a + a + a;
+            if (prod == 1)
+                return true;
+            else
+                return false;
+        }
+        public static bool Calcolo4()
+        {
+            double a = 1.0 / 6.0;
+            double prod = a * 6.0;
+            double s = a + a + a + a + a + a;
+            if (prod == 1)
+                return false;
+            else
+                return true;
+        }
+    }
+}
